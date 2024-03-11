8e398ab04c53abb05ffa960d0f353eb248434998
jomax01 <116906075+jomax01@users.noreply.github.com>
2024-03-11 20:17:20 +0100

    chore : setting database connection and cors

--- tours-service/main.go
+++ tours-service/main.go
@@ -8,21 +8,22 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func initDB() *gorm.DB {
-	connectionStr := "root:root@tcp(localhost:3306)/students?charset=utf8mb4&parseTime=True&loc=Local"
-	database, err := gorm.Open(mysql.Open(connectionStr), &gorm.Config{})
+	connectionStr := "host=localhost user=postgres password=super dbname=gorm port=5432 sslmode=disable"
+	database, err := gorm.Open(postgres.Open(connectionStr), &gorm.Config{})
 	if err != nil {
 		print(err)
 		return nil
 	}
 
 	database.AutoMigrate(&model.Student{})
-	database.Exec("INSERT IGNORE INTO students VALUES ('aec7e123-233d-4a09-a289-75308ea5b7e6', 'Marko Markovic', 'Graficki dizajn')")
+	//database.Exec("INSERT IGNORE INTO students VALUES ('aec7e123-233d-4a09-a289-75308ea5b7e6', 'Marko Markovic', 'Graficki dizajn')")
 	return database
 }
 
@@ -32,9 +33,14 @@ func startServer(handler *handler.StudentHandler) {
 	router.HandleFunc("/students/{id}", handler.Get).Methods("GET")
 	router.HandleFunc("/students", handler.Create).Methods("POST")
 
+	permitedHeaders := handlers.AllowedHeaders([]string{"Requested-With", "Content-Type", "Authorization"})
+	permitedOrigins := handlers.AllowedOrigins([]string{"*"})
+	permitedMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
+
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static")))
 	println("Server starting")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8082", handlers.CORS(permitedHeaders, permitedOrigins, permitedMethods)(router)))
+
 }
 
 func main() {
